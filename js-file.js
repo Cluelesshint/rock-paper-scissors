8e0a7c219185348171ac13770d52683e54b01ddf
Cluelesshint <connorwaggoner98@gmail.com>
2021-10-21 15:10:19 -0400

    add major changes to js file

--- js-file.js
+++ js-file.js
@@ -1,57 +1,49 @@
+const start = document.querySelector('.start-btn');
+const reset = document.querySelector('.reset-btn');
 
+start.addEventListener('click', showYourTurn)
 
+const playerChoice = document.querySelectorAll('.choices')
 
+playerChoice.forEach((e) => {
+    e.addEventListener('click', function(e){
+        playRound(e);
+    })
+});
 
+function showYourTurn(){
+    const turnIndicator = document.querySelector('.turn-indicator');
+    if (turnIndicator.outerText == ''){
+        turnIndicator.textContent = 'Pick One';
+    } 
+}
 
+function playRound(e){
+    const turnIndicator = document.querySelector('.turn-indicator');
+    const checkResult = document.querySelector('.result');
+    if (turnIndicator.textContent == 'Pick One'){
+        turnIndicator.textContent = '';
+        displayComputerResult('Choosing...');
+        setTimeout(function () {displayComputerResult(computerPlay());}, 1000);
+    }
+    else if (checkResult != '') alert('Game in progress!');
+    else alert('Hey, You forgot to click start!');
+
+}
+
+function displayComputerResult(result){
+    const resultDiv = document.querySelector('.result');
+    resultDiv.textContent = result;
+}
 
 
 function choosePlay(num){
-    if (num == 1) return 'rock';
-    else if (num == 2) return 'paper';
-    else return 'scissors';
+    if (num == 1) return 'Rock';
+    else if (num == 2) return 'Paper';
+    else return 'Scissors';
 }
 
 function computerPlay(){
     let rand = Math.floor(Math.random() * 3) + 1;
     return choosePlay(rand);
 }
-
-function getPlayerChoice(){
-    let choice;
-    while (true){
-        choice = prompt('rock, paper, or scissors?')
-        choice.toLowerCase();
-        if (choice == 'rock' || choice == 'paper' || choice == 'scissors'){
-            return choice;
-            break;
-        } 
-        else alert('Invalid Input');
-    }
-}
-
-function playRound(playerChoice, computerChoice){
-    if (playerChoice === computerChoice) return 'It\'s a tie!';
-    switch (playerChoice){
-        case 'rock':
-            if (computerChoice == 'paper'){
-                return 'You lose! Paper beats rock!';
-            }
-            else return 'You win! Rock beats scissors';
-            break;
-        case 'paper':
-            if (computerChoice == 'rock'){
-                return 'You win! Paper beats rock';
-            }
-            else return 'You lose! Scissors beat paper';
-            break;
-        case 'scissors':
-            if (computerChoice == 'rock'){
-                return 'You lose! Rock beats scissors';
-            }
-            else return 'You win! Scissors beats paper';
-            break;
-    } 
-}
-
-
-
